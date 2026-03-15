8ce3759cb8badd5bccbce467e31b724b3d746485
harini-y-ops <harini.n20061@gmail.com>
2026-03-15 11:58:26 +0530

    Update movie_rec.py
    A Python CLI app to recommend movies by genre, rating, random pick, or director search.

--- movie_rec.py
+++ movie_rec.py
@@ -1,5 +1,4 @@
-genre=input("Enter the preferred genre: ").lower()
-rating=float(input("Enter the rating of the movie: "))
+import random
 movies=[
     {"title":"Inception","genre":"sci-fi","director":"Christopher Nolan","rating":8.8},
     {"title":"The Godfather","genre":"crime","director":"Francis Ford Coppola","rating":9.2},
@@ -12,10 +11,43 @@ movies=[
     {"title":"Superbad","genre":"comedy","director":"Greg Mottola","rating":7.6},
     {"title":"Interstellar","genre":"adventure","director":"Christopher Nolan","rating":8.7}
 ]
-recommended=[movie for movie in movies if movie["genre"]==genre and movie ["rating"]>=rating]
-if recommended:
-    print("Recommended movies\n")
-    for movie in recommended:
-        print(f" {movie['title']} — {movie['director']} — {movie['rating']}")
+print("------MENU------")
+print("1.Recommend movie")
+print("2.Random movie")
+print("3.Search by director")
+print("4.Exit")
+choice=input("Enter your choice: ")
+if choice=="1":
+    genre=set(movie["genre"]for movie in movies)
+    print("Available genres:\n", ", ".join(genre))
+    genre=input("Enter the preferred genre: ").lower()
+    rating=float(input("Enter the rating of the movie: "))
+    recommended=[movie for movie in movies if movie["genre"]==genre and movie ["rating"]>=rating]
+    if recommended:
+        print("Recommended movies\n")
+        for movie in recommended:
+            print(f" {movie['title']} — {movie['director']} — {movie['rating']}")
+    else:
+        print("No movies found! Try a different genre")
+    
+elif choice =="2":
+    movie=random.choice(movies)
+    print("Random movie recommendation: ")
+    print(f"{movie['title']}-{movie['genre']}-{movie['rating']}")
+    
+elif choice =="3":
+    director=input("Enter the director name: ").lower()
+    found=False
+    for movie in movies:
+        if movie["director"].lower()==director:
+            print(f"{movie['title']}-{movie['rating']}")
+            found=True
+        if not found:
+            print("No movies from the director")
+        
+         
+elif choice =="4":
+    print("Exiting program!")
+
 else:
-    print("No movies found! Try a different genre")
+    print("Invalid choice please try again")
